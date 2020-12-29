a8b0358dc3c7e955fdccd1f06c81865e2b268515
Benny <meier.benjamin@web.de>
2020-12-29 13:50:52 +0100

    Add react-native-web + react-dom to see the native app in the browser for better development

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('main', () => App);
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
